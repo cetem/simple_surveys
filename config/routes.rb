af82fbc08525187fb6110b9ffa966bc84c0680d0
Néstor Coppi <nestorcoppi@gmail.com>
2013-07-01 19:31:18 -0300

    Ruta para la facu

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 SimpleSurveys::Application.routes.draw do
-  match 'surveys/info' => 'surveys#info', as: 'surveys_info', via: :get
+  scope '/metrotranvia' do
+    match 'surveys/info' => 'surveys#info', as: 'surveys_info', via: :get
 
-  resources :surveys, only: [:index, :show, :new, :create]
+    resources :surveys, only: [:index, :show, :new, :create]
 
-  devise_for :users
-  
-  resources :users do
-    member do
-      get :edit_profile
-      put :update_profile
+    devise_for :users
+    
+    resources :users do
+      member do
+        get :edit_profile
+        put :update_profile
+      end
     end
+    
+    root to: 'surveys#info'
   end
-  
-  root to: 'surveys#info'
 end
